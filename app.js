51c831a137b5e553c7155d1d115a1de6d6239cc8
Adrian Grociak <ajgrociak@gmail.com>
2024-04-18 16:15:13 +0200

    Create client messaging functionality

--- app.js
+++ app.js
@@ -4,3 +4,52 @@ const messagesList = document.querySelector('#messages-list');
 const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
+
+let userName;
+
+const hideMessages = () => {
+  messagesSection.classList.remove('show');
+};
+
+const login = () => {
+  if (!userNameInput.value) alert('Username is empty!');
+  userName = userNameInput.value;
+
+  loginForm.classList.remove('show');
+  messagesSection.classList.add('show');
+};
+
+const addMessage = (author, content) => {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) {
+    message.classList.add('message--self');
+  }
+  message.innerHTML = `
+    <h3 class='message__author'>${userName === author ? 'You' : author }</h3>
+    <div class='message__content'>${content}</div>
+  `;
+  messagesList.appendChild(message);
+};
+
+const sendMessage = () => {
+  if (!messageContentInput.value) {
+    alert('Please enter the message');
+    return;
+  }
+  addMessage(userName, messageContentInput.value);
+  messageContentInput.value = '';
+};
+
+hideMessages();
+
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+  login();
+});
+
+addMessageForm.addEventListener('submit', e => {
+  e.preventDefault();
+  sendMessage();
+});
